4fbdd1323c7182c3fa5a31b824a5c760be0bc6fa
Reyhan Asta <astareyhan@gmail.com>
2025-10-04 12:19:55 +0700

    1 baris di header untuk integrasi dengan ngrok

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 <title>{{ $title ?? config('app.name') }}</title>
 
 <link rel="icon" href="/favicon.ico" sizes="any">
