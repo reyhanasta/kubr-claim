bf4782a600078e3a946ddd8cdc8a4201bc4e48cf
reyhanasta <astareyhan@gmail.com>
2025-12-20 09:16:01 +0700

    Ganti ambil file download dari backup ke main file

--- app/Livewire/ClaimsList.php
+++ app/Livewire/ClaimsList.php
@@ -92,7 +92,7 @@ class ClaimsList extends Component
     public function downloadFile($claimId)
     {
         $claim = BpjsClaim::findOrFail($claimId);
-        $disk = Storage::disk('backup');
+        $disk = Storage::disk('shared');
 
         if (! $disk->exists($claim->file_path)) {
             LivewireAlert::title('File tidak ditemukan')
@@ -120,7 +120,7 @@ class ClaimsList extends Component
             return;
         }
 
-        $disk = Storage::disk('backup');
+        $disk = Storage::disk('shared');
 
         if (! $disk->exists($claim->lip_file_path)) {
             LivewireAlert::title('File tidak ditemukan')
